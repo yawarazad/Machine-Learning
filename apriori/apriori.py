1af15bf4acbe5901fc686296e02b750e99a5f2b3
yawarazad <yawarazad@gmail.com>
2019-11-28 20:20:46 +0530

    initial commmit

---
+++ apriori/apriori.py
@@ -0,0 +1,101 @@
+'''
+    @author: Yawar Azad
+    github: https://github.com/yawarazad
+    Student Id: 201902566
+    MTech CSE (2019-20121)
+
+    APRIORI ALGORITHM
+'''
+def predata(filename,minSup):
+    items = c.defaultdict(set)
+    itemset = set()
+    freitems = set()
+    total_item = []
+    
+    num_basket = 0
+    for i in filename:
+        num_basket += 1
+        i = i.split()
+        i = set(i)
+        total_item +=i
+        items[num_basket]=i
+        itemset = itemset.union(i)
+    
+    total = c.Counter(total_item)
+    for item in itemset:
+        num = total[item]
+        if float(num)/num_basket > minSup:
+            freitems.add(item)
+ 
+    return items, freitems
+
+
+def gencandidate(freitems,k):
+    candidate = set()
+    
+    if k==2:
+        for x in freitems:
+            for y in freitems:
+                if x!=y:
+                    candidate.add((x,y))
+    else:
+        for x in freitems:
+            for y in freitems:
+                if len(set(x).union(y))==k:
+                    candidate.add(tuple(set(x).union(y)))
+        candidate=list(candidate)        
+        for c in candidate:
+            subsets = getsubsets(c)
+            if any( [x not in freitems for x in subsets]):
+                candidate.remove(c)     
+    return set(candidate)
+
+
+def getsubsets(candidate):
+    import itertools
+    subsets=[]
+    subsets.extend(itertools.combinations(candidate,len(candidate)-1))
+    
+    return subsets
+
+
+def genfreitems(candidate,items,minSup):
+    freitems = set()
+    
+    count=0
+    pref=open('pres.txt','a')    
+    for c in candidate:
+        for k in items:
+            if set(c).issubset(items[k]):
+                count+=1
+        sup=float(count)/len(items)
+        if sup > minSup:
+            freitems.add(c)
+            print(c)
+        pref.write(str(c))
+            
+    pref.close()
+    return freitems
+
+
+def main():
+    k=4
+    minSup=0.08
+    f=open("apriori.txt","r")
+    
+    candidate = set()
+    freitems = set()
+    
+    items, freitems = predata(f,minSup)
+    
+    for i in range(2,k+1):
+        candidate = gencandidate(freitems,i)
+        freitems = genfreitems(candidate,items,minSup)
+   
+    return freitems
+
+
+if __name__=='__main__':
+    import collections as c 
+    main()
+    
---
+++ knn/knn.py
@@ -0,0 +1,52 @@
+'''
+    @author: Yawar Azad
+    github: https://github.com/yawarazad
+    Student Id: 201902566
+    MTech CSE (2019-20121)
+
+    K-NEAREST NEIGHBOURS
+'''
+def knn(data, query, k):
+    neighbor_distances_and_indices = []
+    for index, example in enumerate(data):
+        distance = euclidean_distance(example[:-1], query)    
+        neighbor_distances_and_indices.append((distance, index))
+    sorted_neighbor_distances_and_indices = sorted(neighbor_distances_and_indices)
+    k_nearest_distances_and_indices = sorted_neighbor_distances_and_indices[:k]
+    k_nearest_labels = [data[i][1] for distance, i in k_nearest_distances_and_indices]
+    kmean = sum(k_nearest_labels) / len(k_nearest_labels)
+    return k_nearest_distances_and_indices , kmean
+
+def mean(labels):
+    return sum(labels) / len(labels)
+
+def euclidean_distance(point1, point2):
+    sum_squared_distance = 0
+    for i in range(len(point1)):
+        sum_squared_distance += (point1[i] - point2[i]) ** 2
+    return sum_squared_distance ** 0.5
+
+def main():
+    data = [
+       [65.75, 112.99],
+       [71.52, 136.49],
+       [69.40, 153.03],
+       [68.22, 142.34],
+       [67.79, 144.30],
+       [68.70, 123.30],
+       [69.80, 141.49],
+       [70.01, 136.46],
+       [67.90, 112.37],
+       [66.49, 127.45],
+    ]
+    
+    #enter user data
+    x = int(input("Enter data point to classify by knn: "))
+    reg_query = [x]
+    k = int(input("Enter k: "))
+    reg_k_nearest_neighbors, reg_prediction = knn(data, reg_query, k=3)
+    
+    print(reg_prediction)
+
+if __name__ == '__main__':
+    main()
